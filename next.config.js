9d55ded5a103fd20251f52e05495f52144412156
andrewbastian <andrew.bastian@hey.com>
2022-09-09 13:20:05 -0700

    added `envs` to config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+    env: {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    }
 }
 
 module.exports = nextConfig
